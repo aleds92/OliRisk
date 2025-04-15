c5ae50115aeaade645fba1cb22c0f692f50a8e9b
aleds92 <alessandrodesantis.9792@gmail.com>
2025-04-15 12:49:35 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -455,7 +455,6 @@ if generate_report:
     from io import BytesIO
     from reportlab.lib.pagesizes import A4
     from reportlab.pdfgen import canvas
-import plotly.io as pio
 pio.templates.default = "plotly_white"
 pie_image = None
 if filtered_values:
